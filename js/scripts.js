925c7dd61a45c374de6ba4925601c79fb0982602
Andriy Kotiyga <kotygaandrey05@gmail.com>
2022-08-27 23:04:47 +0300

    - added navigation

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,13 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function() {
 	header.classList.toggle("sticky", this.window.scrollY > 0);
 });
+
+const sr = ScrollReveal ({
+	distance: '25px',
+	duration: 2500,
+	reset: true
+})
+
+sr.reveal('.home-text',{delay:190, origin:'bottom'})
+
+sr.reveal('.home,.about,.services,.projects,.contacts',{delay:200, origin:'bottom'})
