4adfb0a92817106b3c8b28f60858e410b14700fd
Andrey5kot <kotygaandrey05@gmail.com>
2022-09-10 19:44:28 +0300

    - adapted header for mobile - created dropdown menu in mobile

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,26 @@ const sr = ScrollReveal ({
 	distance: '25px',
 	duration: 2500,
 	reset: true
-})
+});
 
-sr.reveal('.home-text',{delay:190, origin:'bottom'})
+sr.reveal('.home-text',{ delay:190, origin:'bottom' })
 
-sr.reveal('.home,.about,.services,.projects,.contacts',{delay:200, origin:'bottom'})
+sr.reveal('.home,.about,.services,.projects,.contacts', { delay:200, origin:'bottom' })
+
+onNavigated = () => {
+	changeNavNarVisibility(true);
+}
+
+toggleNavBar = () => {
+	const visibility = document.getElementById("nav-bar").style.visibility;
+	changeNavNarVisibility(visibility === 'visible');
+};
+
+window.onorientationchange = (event) => {
+	const screenSize = event.target.screen.orientation.angle;
+	changeNavNarVisibility(screenSize === 0);
+};
+
+changeNavNarVisibility = (expression) => {
+	document.getElementById("nav-bar").style.visibility = expression ? 'hidden' : 'visible';
+}
